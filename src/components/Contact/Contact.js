b9066454d22a722ab7a8c88255826329b4aa4219
 <96326525+mutasim77@users.noreply.github.com>
2023-12-27 08:14:11 +0600

    feat: add new component Contact

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,8 @@
+const addressInfoData = [
+    {
+        city: "Астана",
+        address: "ул. Ж. Тархана 14",
+        number: "+7 701 101 2525"
+    }
+]
+const contactCard = document.querySelector('.contact-cards');
