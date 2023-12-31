315309db2128eb85ddecf64f5fb1e1d40c7500f3
 <96326525+mutasim77@users.noreply.github.com>
2023-12-31 13:15:48 +0600

    feat: add helper functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,9 @@
 export const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
+export const isEmptyArray = (array) => array.length < 1;
+
+export const getTotalPrice = (array) => array.reduce((acc, curr) => acc + +curr.price, 0);
+
+export const currencyFormat = (number, currency) => {
+    return new Intl.NumberFormat('de-DE', { style: 'currency', currency }).format(number);
+}
